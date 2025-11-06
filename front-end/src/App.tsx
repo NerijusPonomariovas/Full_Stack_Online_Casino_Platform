4a29931e3f070a2710b3dbd1fff3ea2506c5740d
Jakub <113280865+NotM1NE@users.noreply.github.com>
2025-11-06 13:35:14 +0200

    Revert "register/login"
    This reverts commit edda5639bcb7a3d2dfd08f5e12f3b3c6980fd7ba.

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,6 +1,8 @@
 
 import './App.css'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
 import Dashboard from './pages/admin/Dashboard'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
@@ -12,6 +14,8 @@ function App() {
      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
         <Route path="/dashBoard" element={<Dashboard/>}/>
          <Route path="/mines" element={<Mines />} />
       </Routes>
