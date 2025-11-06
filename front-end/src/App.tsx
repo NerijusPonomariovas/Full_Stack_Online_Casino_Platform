2189b10afcb67ba9326d7b76b4c89ad24077ab7d
Jakub <113280865+NotM1NE@users.noreply.github.com>
2025-11-06 13:19:28 +0200

    Revert "Merge branch 'main' of https://github.com/Cataris/Front-end-back-end"
    This reverts commit 8d2dc33e612ae34be23811b36d8a5abe927d90f7, reversing
    changes made to edda5639bcb7a3d2dfd08f5e12f3b3c6980fd7ba.

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -6,7 +6,6 @@ import Register from './pages/Register'
 import Dashboard from './pages/admin/Dashboard'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
-import Mines from './mines'
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/dashBoard" element={<Dashboard/>}/>
-         <Route path="/mines" element={<Mines />} />
       </Routes>
     </BrowserRouter>
     
