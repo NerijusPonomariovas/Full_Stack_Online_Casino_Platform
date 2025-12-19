056da5e20b1fa0029b0c3a2a4902af7a43a18442
thrust0 <salys2005@gmail.com>
2025-12-19 06:38:00 +0200

    modified:   front-end/src/pages/games/catGame/Cat.tsx

--- front-end/src/pages/games/catGame/Cat.tsx
+++ front-end/src/pages/games/catGame/Cat.tsx
@@ -43,7 +43,7 @@ export default function Home() {
   return (  
     <main className="home flex">
       {!isAuthenticated && (
-        <div className="fixed inset-0 bg-gradient-to-b from-[#102c56] via-[#0b3a6f] to-[#081c36] bg-opacity-100 z-10 flex justify-center items-center">
+        <div className="fixed inset-0 bg-linear-to-b from-[#102c56] via-[#0b3a6f] to-[#081c36] bg-opacity-100 z-10 flex justify-center items-center">
           <div className="bg-white p-6 rounded-lg w-11/12 sm:w-96 relative">
             <div className="flex items-center justify-between">
               <div className="flex items-center">
