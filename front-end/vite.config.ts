a68408d9e0243fe243cf5df5f78ddfb27f0434cf
Jakub <113280865+NotM1NE@users.noreply.github.com>
2025-12-19 08:35:29 +0200

    deploy w more images test!

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   server:
@@ -9,6 +10,14 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/*',  // Source directory
+          dest: 'src/assets'         // Destination directory in dist/
+        }
+      ]
+    })
   ]
 })
