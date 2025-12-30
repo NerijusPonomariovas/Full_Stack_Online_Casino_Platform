2a8d2bb52d2bc1e9efdd10c170d3dce9ae36d148
Jakub <jakub.r002@gmail.com>
2025-12-30 18:42:51 +0200

    Enable viteStaticCopy plugin in Vite config
    Uncommented the viteStaticCopy plugin in vite.config.ts to allow static asset copying during build. This change activates the plugin for handling static files in the front-end project.

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-//import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   server:
@@ -11,7 +11,6 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    /*
     viteStaticCopy({
       targets: [
         {
@@ -20,6 +19,5 @@ export default defineConfig({
         }
       ]
     })
-    */
   ]
 })
