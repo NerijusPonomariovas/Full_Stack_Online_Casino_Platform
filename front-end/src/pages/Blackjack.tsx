721f9c5ee9037e8f02048cab2cc2f33a2d457d60
Jakub <jakub.r002@gmail.com>
2026-01-13 17:03:04 +0200

    uiui

--- front-end/src/pages/Blackjack.tsx
+++ front-end/src/pages/Blackjack.tsx
@@ -30,7 +30,6 @@ export default function Home() {
   const [showLogin, setShowLogin] = useState(false);
   const [showRegister, setShowRegister] = useState(false);
   const [balance, setBalance] = useState<number>(0.0);
-  const [xuj, setXuj] = useState(false);
 
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
